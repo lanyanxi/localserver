9ddc2356106e93cfb6bf93488811e79a9bc3e668
lanyanxi <46485949+lanyanxi@users.noreply.github.com>
2019-07-09 10:07:20 +0800

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,112 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    meuns: [
+      {
+        name: '美食',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/foods.png'
+      },
+      {
+        name: '出行',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/travel.png'
+      },
+      {
+        name: '酒店',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/hotel.png'
+      },
+      {
+        name: '旅游',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/attractions.png'
+      },
+      {
+        name: '外卖',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/takeaway.png'
+      },
+      {
+        name: '景点',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/tourism.png'
+      },
+      {
+        name: '摄影',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/photography.png'
+      },
+      {
+        name: '娱乐',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/entertainment.png'
+      },
+      {
+        name: '更多',
+        url: '/pages/index/index',
+        icon: '../assets/images/meun/more.png'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
